4490b165a4eb26b0794f59b6f99a773c7f4a2393
Pavan Chavan <69139294+Pavan-Chavan@users.noreply.github.com>
2023-09-24 11:51:46 +0530

    Time: 2 ms (74.76%), Space: 43.6 MB (86.26%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.java
+++ 0039-combination-sum/0039-combination-sum.java
@@ -1,24 +1,20 @@
 class Solution {
-    private void helper(int[] candidates,int target,List<List<Integer>> result,List<Integer> CurrArr,int currSum, int currIndex){
-        if(currSum == target ){
-            result.add(new ArrayList<>(CurrArr));
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        List<List<Integer>> result = new ArrayList<>();
+        helper(result,new ArrayList<>(), candidates, target,0,0);
+        return result;
+    }
+    private void helper(List<List<Integer>> result, List<Integer>tempList, int[] candidates, int target, int start,int currSum) {
+        if(currSum == target) {
+            result.add(new ArrayList<>(tempList));
             return;
         } else if (currSum > target) return;
         else {
-            for(int i = currIndex;i<candidates.length;i++) {
-                CurrArr.add(candidates[i]);
-                currSum = currSum + candidates[i];
-                helper(candidates,target,result,CurrArr,currSum,i);
-                CurrArr.remove(CurrArr.size() - 1);
-                currSum = currSum - candidates[i];
+            for(int i=start; i<candidates.length; i++) {
+                tempList.add(candidates[i]);
+                helper(result,tempList, candidates, target,i,currSum+candidates[i]);
+                tempList.remove(tempList.size()-1);
             }
-        } 
-    }
-
-    public List<List<Integer>> combinationSum(int[] candidates, int target) {
-        List<List<Integer>> result = new ArrayList<>();
-        List<Integer> CurrArr = new ArrayList<>();
-        helper(candidates,target,result,CurrArr,0,0);
-        return result;
+        }
     }
 }
